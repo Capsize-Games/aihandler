6982aa0d522ca457401dac862fae5b31bdf199eb
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-05-18 19:34:37 -0600

    adds ability to merge models

--- src/aihandler/runner.py
+++ src/aihandler/runner.py
@@ -526,11 +526,28 @@ class SDRunner(BaseRunner):
             self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
             self.pipe.enable_model_cpu_offload()
 
-    def _load_ckpt_model(self):
-        logger.debug(f"Loading ckpt file, is safetensors {self.is_safetensors}")
+    def _load_ckpt_model(
+        self, 
+        path=None, 
+        is_controlnet=False,
+        is_safetensors=False,
+        data_type=None,
+        do_nsfw_filter=False,
+        device=None,
+        scheduler_name=None
+    ):
+        logger.debug(f"Loading ckpt file, is safetensors {is_safetensors}")
+        if not data_type:
+            data_type = self.data_type
         try:
-            pipeline = self.download_from_original_stable_diffusion_ckpt()
-            if self.is_controlnet:
+            pipeline = self.download_from_original_stable_diffusion_ckpt(
+                path=path,
+                is_safetensors=is_safetensors,
+                do_nsfw_filter=do_nsfw_filter,
+                device=device,
+                scheduler_name=scheduler_name
+            )
+            if is_controlnet:
                 pipeline = self.load_controlnet_from_ckpt(pipeline)
         except Exception as e:
             print("Something went wrong loading the model file", e)
@@ -538,17 +555,25 @@ class SDRunner(BaseRunner):
             raise e
         # to half
         # determine which data type to move the model to
-        pipeline.vae.to(self.data_type)
-        pipeline.text_encoder.to(self.data_type)
-        pipeline.unet.to(self.data_type)
+        pipeline.vae.to(data_type)
+        pipeline.text_encoder.to(data_type)
+        pipeline.unet.to(data_type)
         if self.do_nsfw_filter:
             pipeline.safety_checker.half()
         return pipeline
 
-    def download_from_original_stable_diffusion_ckpt(self, config="v1.yaml"):
+    def download_from_original_stable_diffusion_ckpt(
+        self, 
+        config="v1.yaml", 
+        path=None,
+        is_safetensors=False,
+        scheduler_name=None,
+        do_nsfw_filter=False,
+        device=None
+    ):
         from diffusers.pipelines.stable_diffusion.convert_from_ckpt import \
             download_from_original_stable_diffusion_ckpt
-        print("is safetensors", self.is_safetensors)
+        print("is safetensors", is_safetensors)
         schedulers = {
             "Euler": "euler",
             "Euler a": "euler-ancestral",
@@ -565,14 +590,20 @@ class SDRunner(BaseRunner):
             "DPM2 a k": "dpm2ak",
             "DEIS": "deis",
         }
+        if not scheduler_name:
+            scheduler_name = self.scheduler_name
+        if not path:
+            path = f"{self.settings_manager.settings.model_base_path.get()}/{self.model}"
+        if not device:
+            device = self.device
         try:
             return download_from_original_stable_diffusion_ckpt(
-                checkpoint_path=f"{self.settings_manager.settings.model_base_path.get()}/{self.model}",
+                checkpoint_path=path,
                 original_config_file=config,
-                scheduler_type=schedulers[self.scheduler_name],
-                device=self.device,
-                from_safetensors=self.is_safetensors,
-                load_safety_checker=self.do_nsfw_filter,
+                scheduler_type=schedulers[scheduler_name],
+                device=device,
+                from_safetensors=is_safetensors,
+                load_safety_checker=do_nsfw_filter,
             )
         # find exception: RuntimeError: Error(s) in loading state_dict for UNet2DConditionModel
         except RuntimeError as e:
@@ -610,7 +641,11 @@ class SDRunner(BaseRunner):
             logger.debug("Loading model from scratch")
             if self.is_ckpt_model or self.is_safetensors:
                 logger.debug("Loading ckpt or safetensors model")
-                self.pipe = self._load_ckpt_model()
+                self.pipe = self._load_ckpt_model(
+                    is_controlnet=self.is_controlnet,
+                    is_safetensors=self.is_safetensors,
+                    do_nsfw_filter=self.do_nsfw_filter
+                )
             else:
                 logger.debug("Loading from diffusers pipeline")
                 if self.is_controlnet:
@@ -1579,3 +1614,142 @@ class SDRunner(BaseRunner):
 
     def cancel(self):
         self.do_cancel = True
+
+    def merge_models(self, base_model_path, models_to_merge_path, weights, output_path, name):
+        from diffusers import (
+            DiffusionPipeline,
+            StableDiffusionPipeline,
+            StableDiffusionImg2ImgPipeline,
+            StableDiffusionInstructPix2PixPipeline,
+            StableDiffusionInpaintPipeline,
+            StableDiffusionDepth2ImgPipeline,
+            StableDiffusionUpscalePipeline,
+            StableDiffusionControlNetPipeline
+        )
+        # load base_model using pretrained
+        if base_model_path.endswith('.ckpt'):
+            pipe = StableDiffusionInpaintPipeline.from_pretrained(
+                "facebookresearch/DeOldify:main",
+                local_files_only=self.local_files_only
+            )
+            pipe.vae.load_state_dict(torch.load(base_model_path))
+        else:
+            pipe = StableDiffusionInpaintPipeline.from_pretrained(
+                base_model_path,
+                local_files_only=self.local_files_only
+            )
+        # vae = pipe.vae
+        # unet = pipe.unet
+        # text_encoder = pipe.text_encoder
+        # load models_to_merge
+        for index in range(len(models_to_merge_path)):
+            weight = weights[index]
+            model_path = models_to_merge_path[index]
+            if model_path.endswith('.ckpt'):
+                model = self._load_ckpt_model(model_path, scheduler_name="Euler a")
+            else:
+                model =type(pipe).from_pretrained(model_path, local_files_only=self.local_files_only)
+
+            pipe.vae = self.merge_vae(pipe.vae, model.vae, weight)
+            pipe.unet = self.merge_unet(pipe.unet, model.unet, weight)
+            pipe.text_encoder = self.merge_text_encoder(pipe.text_encoder, model.text_encoder, weight)
+        # save model
+        # pipe.vae = vae
+        # pipe.unet = unet
+        # pipe.text_encoder = text_encoder
+        output_path = os.path.join(output_path, name)
+        print(f"Saving to {output_path}")
+        pipe.save_pretrained(output_path)
+        print("merge complete")
+    
+    def merge_vae(self, vae_a, vae_b, weight_b=0.6):
+        """
+        Merge two VAE models by averaging their weights.
+
+        Args:
+            vae_a (nn.Module): First VAE model.
+            vae_b (nn.Module): Second VAE model.
+            weight_b (float): Weight to give to the second model. Default is 0.6.
+
+        Returns:
+            nn.Module: Merged VAE model.
+        """
+        # Get the state dictionaries of the two VAE models
+        state_dict_a = vae_a.state_dict()
+        state_dict_b = vae_b.state_dict()
+
+        # Only merge parameters that have the same shape in both models
+        merged_state_dict = {}
+        for key in state_dict_a.keys():
+            if key in state_dict_b and state_dict_a[key].shape == state_dict_b[key].shape:
+                merged_state_dict[key] = (1 - weight_b) * state_dict_a[key] + weight_b * state_dict_b[key]
+            else:
+                print("shape does not match")
+                merged_state_dict[key] = state_dict_a[key]
+
+        # Load the merged state dictionary into a new VAE model
+        merged_vae = type(vae_a)()
+        vae_a.load_state_dict(merged_state_dict)
+
+        return vae_a
+
+    def merge_unet(self, unet_a, unet_b, weight_b=0.6):
+        """
+        Merge two U-Net models by averaging their weights.
+
+        Args:
+            unet_a (nn.Module): First U-Net model.
+            unet_b (nn.Module): Second U-Net model.
+            weight_b (float): Weight to give to the second model. Default is 0.6.
+
+        Returns:
+            nn.Module: Merged U-Net model.
+        """
+        # Get the state dictionaries of the two U-Net models
+        state_dict_a = unet_a.state_dict()
+        state_dict_b = unet_b.state_dict()
+
+        # Average the weights of the two models, giving more weight to unet_b
+        merged_state_dict = {}
+        for key in state_dict_a.keys():
+            if key in state_dict_b and state_dict_a[key].shape == state_dict_b[key].shape:
+                merged_state_dict[key] = (1 - weight_b) * state_dict_a[key] + weight_b * state_dict_b[key]
+            else:
+                print("shape does not match")
+                merged_state_dict[key] = state_dict_a[key]
+
+        # Load the averaged weights into a new U-Net model
+        merged_unet = type(unet_a)()
+        unet_a.load_state_dict(merged_state_dict)
+
+        return unet_a
+
+    def merge_text_encoder(self, text_encoder_a, text_encoder_b, weight_b=0.6):
+        """
+        Merge two Text Encoder models by averaging their weights.
+
+        Args:
+            text_encoder_a (nn.Module): First Text Encoder model.
+            text_encoder_b (nn.Module): Second Text Encoder model.
+            weight_b (float): Weight to give to the second model. Default is 0.6.
+
+        Returns:
+            nn.Module: Merged Text Encoder model.
+        """
+        # Get the state dictionaries of the two Text Encoder models
+        state_dict_a = text_encoder_a.state_dict()
+        state_dict_b = text_encoder_b.state_dict()
+
+        # Average the weights of the two models, giving more weight to text_encoder_b
+        merged_state_dict = {}
+        for key in state_dict_a.keys():
+            if key in state_dict_b and state_dict_a[key].shape == state_dict_b[key].shape:
+                merged_state_dict[key] = (1 - weight_b) * state_dict_a[key] + weight_b * state_dict_b[key]
+            else:
+                print("shape does not match")
+                merged_state_dict[key] = state_dict_a[key]
+
+        # Load the averaged weights into a new Text Encoder model
+        text_encoder_a.load_state_dict(merged_state_dict)
+
+        return text_encoder_a
