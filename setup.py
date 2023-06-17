c4446cbe8b89105573efdb7e45f7f12eebe1d087
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2023-06-17 16:44:33 -0600

    bump version to 1.16.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ install_requires = [
 
 setup(
     name="aihandler",
-    version="1.15.8",
+    version="1.16.0",
     author="Capsize LLC",
     description="AI Handler: An engine which wraps certain huggingface models",
     long_description=open("README.md", "r", encoding="utf-8").read(),
